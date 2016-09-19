ae78ecb00bbc61707ad01aecc79f85ac0b929d62
Qingfei.You <qingfeiyou@gmail.com>
2016-09-19 21:50:33 +0800

    feat(java): add solution for binary watch

---
+++ binary_watch.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public List<String> readBinaryWatch(int num) {
+        List<String> result = new ArrayList<>();
+        for (int h = 0; h < 12; ++h) {
+            for (int m = 0; m < 60; ++m) {
+                if (Integer.bitCount(h) +Integer.bitCount(m) == num) {
+                    result.add(String.format("%d:%02d", h, m));
+                }
+            }
+        }
+        return result;
+    }
+}
