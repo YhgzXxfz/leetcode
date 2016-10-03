10b1b306d0af74de9eefda657d6006f37ba856ee
Qingfei.You <qingfeiyou@gmail.com>
2016-10-03 20:53:39 +0800

    feat(cpp): add solution for convert a number to hexadecimal

---
+++ convert_a_number_to_hexadecimal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string toHex(int num) {
+        if(!num) return "0";
+        
+        string result = "";
+        int count = 0;
+        while (num && count < 8)
+        {
+            int temp = num & 15;
+            if (temp<10) result.push_back('0'+ temp);
+            else result.push_back('a'+temp-10);
+            
+            num >>= 4;
+            count++;
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
