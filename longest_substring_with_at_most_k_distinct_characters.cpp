d7133cc0e651a3a46fb8a04634e061950b9d1429
Qingfei.You <qingfeiyou@gmail.com>
2016-11-28 21:38:40 +0100

    feat(cpp): add solution for longest substring with at most k distinct characters

---
+++ longest_substring_with_at_most_k_distinct_characters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int count[256] = {0}; // initialization is necessary for counting distinct numbers
+        int begin = 0, distinct = 0, max_len = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (count[s[i]] == 0) distinct++;
+            count[s[i]]++;
+            
+            while (distinct > k) {
+                count[s[begin]]--;
+                if (count[s[begin]] == 0) distinct--;
+                begin++;
+            }
+            
+            max_len = max(max_len, i-begin+1);
+        }
+        return max_len;
+    }
+};
