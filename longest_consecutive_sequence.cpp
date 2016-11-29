2653a1e70c11d2f6a454caf49110e58b1ca8021b
Qingfei.You <qingfeiyou@gmail.com>
2016-11-29 21:56:43 +0100

    feat(cpp): add solution for longest consecutive sequence

---
+++ longest_consecutive_sequence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> st;
+        for (auto num : nums) {
+            st.insert(num);
+        }
+        
+        int max_len = 0;
+        while(!st.empty()) {
+            int i = *st.begin();
+            st.erase(i);
+            
+            int upper = i+1;
+            while (st.count(upper)) {
+                st.erase(upper);
+                ++upper;
+            }
+            
+            int lower = i-1;
+            while (st.count(lower)) {
+                st.erase(lower);
+                --lower;
+            }
+            
+            max_len = max(max_len, upper-lower -1);
+        }
+        return max_len;
+    }
+};
