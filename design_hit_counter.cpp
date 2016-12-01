edf882a237d91ad0c15ed1e709ef21e073980097
Qingfei.You <qingfeiyou@gmail.com>
2016-12-01 23:02:54 +0100

    feat(cpp): add solution for design hit counter

---
+++ design_hit_counter.cpp
@@ -0,0 +1,33 @@
+class HitCounter {
+public:
+    /** Initialize your data structure here. */
+    HitCounter() {
+        
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    void hit(int timestamp) {
+        mp[timestamp]++;
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    int getHits(int timestamp) {
+        int sum = 0;
+        for (auto entry : mp) {
+            if (entry.first > timestamp-300 && entry.first <=timestamp) sum += entry.second;
+        }
+        return sum;
+    }
+    
+private:
+    unordered_map<int, long> mp;
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
