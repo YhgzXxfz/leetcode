bb3320105c7fde8a500030e5f47bc5f39bccf054
Qingfei.You <qingfeiyou@gmail.com>
2016-12-04 18:19:40 +0100

    feat(cpp): add solution for perfect squares

---
+++ perfect_squares.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        // 1 dp
+        /*
+        if (n <= 0) return 0;
+        
+        vector<int> dp(n+1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j*j <= i; ++j) {
+                dp[i] = min(dp[i], dp[i-j*j]+1);
+            }
+        }
+        return dp[n];
+        */
+        
+        // 2 math return 4 if and only if n = 4^k * (8m+7)
+        if (isSquare(n)) return 1;
+        
+        while ((n&3) == 0) {
+            n >>= 2;
+        }
+        
+        if ((n&7) == 7) return 4;
+        
+        int root = sqrt(n);
+        for (int i = 1; i <= root; ++i) {
+            if (isSquare(n-i*i)) return 2;
+        }
+        return 3;
+        
+        
+    }
+    
+private:
+    bool isSquare(int n) {
+        int root = sqrt(n);
+        return root*root == n;
+    }
+};
