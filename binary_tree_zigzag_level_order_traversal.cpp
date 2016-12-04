531e1c8a2209718fdaf5cba071884f9d50b970c6
Qingfei.You <qingfeiyou@gmail.com>
2016-12-04 19:21:59 +0100

    feat(cpp): add solution for binary tree zigzag level order traversal

---
+++ binary_tree_zigzag_level_order_traversal.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        bfs(root, result, 1, true);
+        return result;
+    }
+    
+    void bfs(TreeNode* root, vector<vector<int>>& result, int level, bool lToR) {
+        if (!root) return;
+        
+        if (level > result.size()) result.push_back(vector<int>());
+        
+        if (lToR) result[level-1].push_back(root->val);
+        else result[level-1].insert(result[level-1].begin(), root->val);
+        
+        bfs(root->left, result, level+1, !lToR);
+        bfs(root->right, result, level+1, !lToR);
+    }
+};
