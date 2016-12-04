daa58585b1bba1bcbac6280ae1024aca5b145a07
Qingfei.You <qingfeiyou@gmail.com>
2016-12-04 19:39:21 +0100

    feat(cpp): add solution for symmetric tree

---
+++ symmetric_tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        
+        auto left = root->left, right = root->right;
+        return isSymmetric(left, right);
+    }
+    
+    bool isSymmetric(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (p && !q) return false;
+        if (!p && q) return false;
+        
+        if (p->val != q->val) return false;
+        
+        auto outer = isSymmetric(p->left, q->right);
+        auto inner = isSymmetric(p->right, q->left);
+        
+        return outer && inner;
+    }
+};
