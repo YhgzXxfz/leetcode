8d4017914a22d419da70eb67d636a604e147b340
Qingfei.You <qingfeiyou@gmail.com>
2016-12-08 20:35:47 +0100

    feat(cpp): add solution for binary tree postorder traversal

---
+++ binary_tree_postorder_traversal.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (!root) return result;
+        postorder(root, result);
+        return result;
+    }
+    
+private:
+    void postorder(TreeNode* root, vector<int>& result) {
+        if (!root) return;
+        postorder(root->left, result);
+        postorder(root->right, result);
+        result.push_back(root->val);
+    }
+};
