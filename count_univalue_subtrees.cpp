87773f7e3e752962933f95577c8063903e5756b6
Qingfei.You <qingfeiyou@gmail.com>
2016-12-08 20:50:32 +0100

    feat(cpp): add solution for count univalue subtrees

---
+++ count_univalue_subtrees.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int countUnivalSubtrees(TreeNode* root) {
+        if (!root) return 0;
+        
+        int count;
+        helper(root, count);
+        return count;
+    }
+    
+private:
+    bool helper(TreeNode* root, int& count) {
+        if (!root) return true;
+        
+        auto left = helper(root->left, count);
+        auto right = helper(root->right, count);
+        
+        if (left && right 
+            && (!root->left || root->left->val == root->val) 
+            && (!root->right || root->right->val == root->val)) {
+            count++;
+            return true;
+        }
+        return false;
+    }
+};
