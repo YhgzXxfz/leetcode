4ef0b8d9549db2785bddba74fca99c4194bfc3ef
Qingfei.You <qingfeiyou@gmail.com>
2016-12-10 12:05:54 +0100

    feat(cpp): add solution for single number iii

---
+++ single_number_iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int sum = 0;
+        for (auto num : nums) {
+            sum ^= num;
+        }
+        int index = findFirstDiff(sum);
+        int num1 = sum, num2 = sum;
+        for (auto num : nums) {
+            if ((num>>index) & 1) num1 ^= num;
+            else num2 ^= num;
+        }
+        return {num1, num2};
+    }
+
+private:
+    int findFirstDiff(int n) {
+        for (int i = 0; i < 32; ++i) {
+            if ((n>>i) & 1) return i;
+        }
+        return 0;
+    }
+};
