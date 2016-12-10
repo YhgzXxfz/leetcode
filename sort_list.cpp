7a7ecc00f8e277db29f75dccba67918e782a9a99
Qingfei.You <qingfeiyou@gmail.com>
2016-12-10 22:51:46 +0100

    feat(cpp): add solution for sort list

---
+++ sort_list.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) return head;
+        auto mid = cutMiddle(head);
+        auto left = sortList(head);
+        auto right = sortList(mid);
+        
+        return merge(left, right);
+    }
+    
+    ListNode* cutMiddle(ListNode* head) {
+        if (!head) return head;
+        auto fast = head, slow = head;
+        while (fast && fast->next && fast->next->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        auto node = slow->next;
+        slow->next = nullptr;
+        return node;
+    }
+    
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        if (l1->val < l2->val) {
+            l1->next = merge(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = merge(l1, l2->next);
+            return l2;
+        }
+    }
+};
