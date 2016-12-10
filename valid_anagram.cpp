76b85b959ef12d17a82b6fdb5c9f7b29ca8f4692
Qingfei.You <qingfeiyou@gmail.com>
2016-12-10 22:06:33 +0100

    feat(cpp): add solution for valid anagram

---
+++ valid_anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        
+        int alphabet[26] = {0};
+        for (int i = 0; i < s.size(); ++i) {
+            alphabet[s[i]-'a']++;
+            alphabet[t[i]-'a']--;
+        }
+        for (int i = 0; i < 26; ++i) {
+            if (alphabet[i]) return false;
+        }
+        return true;
+    }
+};
