495ea0ac92c60986be41e1c58c8906f87fdc6876
Qingfei.You <qingfeiyou@gmail.com>
2016-12-11 11:13:55 +0100

    refactor(cpp): use bit manipulation to determine odd/even for wiggle sort

--- wiggle_sort.cpp
+++ wiggle_sort.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
         for (int i = 1; i < nums.size(); ++i) {
-            if (i%2 && nums[i] < nums[i-1] || (i%2 == 0) && nums[i] > nums[i-1]) swap(nums[i], nums[i-1]);
+            if ((i&1) && nums[i] < nums[i-1] || !(i&1) && nums[i] > nums[i-1]) swap(nums[i], nums[i-1]);
         }
     }
 };
