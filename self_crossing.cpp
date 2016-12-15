e21516b6b69dacf7501d8797c587718a7597edca
Qingfei.You <qingfeiyou@gmail.com>
2016-12-15 08:52:29 +0100

    feat(cpp): add solution for self crossing

---
+++ self_crossing.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSelfCrossing(vector<int>& x) {
+        int len = x.size();
+        if (len < 4) return false;
+        
+        for (int i = 3; i < len; ++i) {
+            if (x[i] >= x[i-2] && x[i-1] <= x[i-3]) return true;
+            if (i >= 4) {
+                if (x[i-1] == x[i-3] && x[i]+x[i-4] >= x[i-2]) return true;
+            }
+            if (i >= 5) {
+                if (x[i-1] <= x[i-3] && x[i-1]+x[i-5] >= x[i-3] 
+                    && x[i-2] >= x[i-4] && x[i]+x[i-4] >= x[i-2]) return true;
+            }
+        }
+        return false;
+    }
+};
