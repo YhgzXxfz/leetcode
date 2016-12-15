797dee25997f14fd260e7f4d19a8b6090f3e5067
Qingfei.You <qingfeiyou@gmail.com>
2016-12-16 00:21:20 +0100

    feat(cpp): add solution for is palindrome

---
+++ is_palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        int rev = reverse(x);
+        return rev == x;
+    }
+    
+private:
+    int reverse(int x) {
+        long long result = 0;
+        while (x) {
+            result = result*10 + x%10;
+            x /= 10;
+            
+            if (result > INT_MAX || result < INT_MIN) return 0;
+        }
+        return result;
+    }
+};
