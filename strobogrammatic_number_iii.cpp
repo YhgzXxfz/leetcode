567ca8b87323f209d4555f6e4f353134423054e9
Qingfei.You <qingfeiyou@gmail.com>
2016-12-18 19:17:48 +0100

    feat(cpp): add solution for strobogrammatic number iii

---
+++ strobogrammatic_number_iii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int strobogrammaticInRange(string low, string high) {
+        vector<int> count = {0};
+        for (int len = low.size(); len <= high.size(); ++len) {
+            string c(len, '\0');
+            dfs(low, high, c, count, 0, len-1);
+        }
+        return count[0];
+    }
+    
+private:
+    void dfs(string low, string high, string c, vector<int>& count, int left, int right) {
+        if (left > right) {
+            if (c.size() == low.size() && c<low || c.size() == high.size() && c > high) return;
+            count[0]++;
+            return;
+        }
+        for (auto& p : pairs) {
+            c[left] = p[0];
+            c[right] = p[1];
+            if (c.size() != 1 && c[0] == '0') continue;
+            if (left == right && p[0] != p[1]) continue;
+            dfs(low, high, c, count, left+1, right-1);
+        }
+    }
+    
+    vector<vector<char>> pairs = {{'0','0'},{'1','1'},{'8','8'},{'9','6'},{'6','9'}};
+};
