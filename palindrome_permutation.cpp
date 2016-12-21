a74daba89ef8b2a802617022f31f93a5b2515fd2
Qingfei.You <qingfeiyou@gmail.com>
2016-12-21 21:07:54 +0100

    refactor(cpp): simplify solution for palindrome permutation

--- palindrome_permutation.cpp
+++ palindrome_permutation.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool canPermutePalindrome(string s) {
-        vector<int> count(256, 0);
+        int count[256] = {0};
         for (auto c : s) {
             count[c]++;
         }
         int odd_occurance = 0;
         for (int i = 0; i < 256; i++) {
-            if (count[i] % 2 != 0) odd_occurance++;
+            if (count[i] &1) odd_occurance++;
             if (odd_occurance > 1) return false;
         }
         return true;
