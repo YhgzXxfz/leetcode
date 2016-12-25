d57cd8ec214a13ca4ae86cfe68db8b0de7f753da
Qingfei.You <qingfeiyou@gmail.com>
2016-12-25 16:51:36 +0100

    feat(cpp); add solution for reverse nodes in k group

---
+++ reverse_nodes_in_k_group.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || !head->next) return head;
+        
+        int count = 0;
+        ListNode fakeHead(0);
+        fakeHead.next = head;
+        auto prev = &fakeHead, curr = head;
+        while (curr) {
+            count++;
+            if (count % k == 0) {
+                auto last = prev->next, next = curr->next;
+                prev->next = reverse(prev->next, curr->next);
+                last->next = next;
+                prev = last;
+                curr = next;
+            } else {
+                curr = curr->next;
+            }
+        }
+        return fakeHead.next;
+    }
+    
+    ListNode* reverse(ListNode* begin, ListNode* end) {
+        if (!begin || begin == end || begin->next == end) return begin;
+        
+        auto next = begin->next;
+        auto rev = reverse(next, end);
+        next->next = begin;
+        begin->next = nullptr;
+        return rev;
+    }
+};
