742bdb6abb878e540a87fbfbc4c3bd52f4e8d3d5
Qingfei.You <qingfeiyou@gmail.com>
2016-12-27 22:13:51 +0100

    refactor(cpp): format solution for word ladder ii

--- word_ladder_ii.cpp
+++ word_ladder_ii.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, unordered_set<string> &wordList) {
-        vector<vector<string> > paths;
+        vector<vector<string>> result;
         vector<string> path(1, beginWord);
         if (beginWord == endWord) {
-            paths.push_back(path);
-            return paths;
+            result.emplace_back(path);
+            return result;
         }
         
         unordered_set<string> words1, words2;
         words1.insert(beginWord);
         words2.insert(endWord);
         
-        unordered_map<string, vector<string> > nexts;
+        unordered_map<string, vector<string>> mp;
         bool words1_is_begin = false;
         
-        if (findLaddersHelper(words1, words2, wordList, nexts, words1_is_begin)) 
-            getPath(beginWord, endWord, nexts, path, paths);
-        return paths;
+        if (dfs(words1, words2, wordList, mp, words1_is_begin)) 
+            getPath(beginWord, endWord, mp, path, result);
+            
+        return result;
     }
     
 private:
-    bool findLaddersHelper(
-        unordered_set<string>& words1,
-        unordered_set<string>& words2,
-        unordered_set<string>& dict,
-        unordered_map<string, vector<string> >& nexts,
-        bool& words1_is_begin) {
+    bool dfs(unordered_set<string>& words1, unordered_set<string>& words2, unordered_set<string>& dict, unordered_map<string, vector<string>>& mp, bool& words1_is_begin) {
+        
         words1_is_begin = !words1_is_begin;
         if (words1.empty()) return false;
-        if (words1.size() > words2.size()) return findLaddersHelper(words2, words1, dict, nexts, words1_is_begin);
-        for (auto word : words1)
-            dict.erase(word);
-        for (auto word : words2)
-            dict.erase(word);
+        if (words1.size() > words2.size()) return dfs(words2, words1, dict, mp, words1_is_begin);
+        for (auto word : words1) dict.erase(word);
+        for (auto word : words2) dict.erase(word);
         
         unordered_set<string> words3;
         bool reach = false;
@@ -45,32 +40,29 @@ private:
                     if (*ch != tmp) {
                         if (words2.find(word) != words2.end()) {
                             reach = true;
-                            words1_is_begin ? nexts[*it].push_back(word) : nexts[word].push_back(*it);
+                            words1_is_begin ? mp[*it].push_back(word) : mp[word].push_back(*it);
                         } else if (!reach && dict.find(word) != dict.end()) {
                             words3.insert(word);
-                            words1_is_begin ? nexts[*it].push_back(word) : nexts[word].push_back(*it);
+                            words1_is_begin ? mp[*it].push_back(word) : mp[word].push_back(*it);
                         }
                     }
                 }
                 *ch = tmp;
             }
         }
-        return reach || findLaddersHelper(words2, words3, dict, nexts, words1_is_begin);
+        return reach || dfs(words2, words3, dict, mp, words1_is_begin);
     }
     
-    void getPath(
-        string beginWord,
-        string &endWord,
-        unordered_map<string, vector<string> >& nexts,
-        vector<string> &path,
-        vector<vector<string> >& paths) {
-        if (beginWord == endWord) paths.push_back(path);
-        else {
-            for (auto next : nexts[beginWord]) {
-                path.push_back(next);
-                getPath(next, endWord, nexts, path, paths);
-                path.pop_back();
-            }
+    void getPath(string begin, string &end, unordered_map<string, vector<string>>& mp, vector<string> &path, vector<vector<string>>& result) {
+        if (begin == end) {
+            result.push_back(path);
+            return ;
+        }
+        
+        for (auto next : mp[begin]) {
+            path.push_back(next);
+            getPath(next, end, mp, path, result);
+            path.pop_back();
         }
     }
 };
