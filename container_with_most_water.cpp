259e935152c1556710cdfcc87238f9d0f7595343
Qingfei.You <qingfeiyou@gmail.com>
2016-12-31 01:06:07 +0100

    feat(cpp): add solution for container with most water

---
+++ container_with_most_water.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int water = 0;
+        int left = 0, right = height.size() - 1;
+        while (left < right) {
+            int h = min(height[left], height[right]);
+            water = max(water, (right-left) * h);
+            
+            while (height[left] <= h && left < right) left++;
+            while (height[right] <= h && left < right) right--;
+        }
+        return water;
+    }
+};
