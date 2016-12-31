8b6ed3a241b6047b66af49508d8ca8f90d65026d
Qingfei.You <qingfeiyou@gmail.com>
2016-12-31 12:43:29 +0100

    feat(cpp): add solution for reverse vowels of a string

---
+++ reverse_vowels_of_a_string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        unordered_set<char> vowels({'a','e','o','i','u','A','E','O','I','U'});
+        
+        int left = 0, right = s.size()-1;
+        while (left < right) {
+            while (left < right && !vowels.count(s[left])) left++;
+            if (left >= right) break;
+            
+            while (right > left && !vowels.count(s[right])) right--;
+            if (left >= right) break;
+            
+            swap(s[left], s[right]);
+            left++; right--;
+        }
+        return s;
+    }
+};
