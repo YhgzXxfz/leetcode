3f2aa38de823bc1e01ff029e37f58e16cc722981
Qingfei.You <qingfeiyou@gmail.com>
2017-01-02 22:10:33 +0100

    feat(cpp): add solution for search in rotated sorted array ii

---
+++ search_in_rotated_sorted_array_ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int len = nums.size(), left = 0, right = len-1;
+        while (left < right) {
+            int mid = left+(right-left)/2;
+            if (nums[mid] == target) return true;
+            
+            if (nums[mid] > nums[right]) {
+                if (nums[left] <= target && target < nums[mid]) right = mid;
+                else left = mid+1;
+            } else if (nums[mid] < nums[right]) {
+                if (nums[mid] < target && target <= nums[right]) left = mid+1;
+                else right = mid;
+            } else {
+                right--;
+            }
+        }
+        return nums[left] == target ? true : false;
+    }
+};
