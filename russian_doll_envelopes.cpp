46dad5315f1731bf971786ff98dfd8b6097412cf
Qingfei.You <qingfeiyou@gmail.com>
2017-01-03 21:38:24 +0100

    feat(cpp): add solution for russian doll envelopes

---
+++ russian_doll_envelopes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+        if (envelopes.empty()) return 0;
+        if (envelopes.size() == 1) return 1;
+        
+        auto comp = [&](pair<int, int> e1, pair<int, int> e2) { 
+            if (e1.first == e2.first) return e1.second < e2.second;
+            return e1.first < e2.first; 
+        };
+        sort(envelopes.begin(), envelopes.end(), comp);
+        
+        int max_count = INT_MIN;
+        int len = envelopes.size();
+        vector<int> dp(len, 1);
+        for (int i = 0; i < len; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (envelopes[j].first < envelopes[i].first && envelopes[j].second < envelopes[i].second) {
+                    dp[i] = max(dp[i], 1+dp[j]);
+                }
+            }
+            max_count = max(max_count, dp[i]);
+        }
+        return max_count;
+    }
+};
