1c548fe00c85c5facd433f8d44bda629b122b068
Qingfei.You <qingfeiyou@gmail.com>
2017-01-06 00:17:10 +0100

    feat(cpp): add solution for triangle

---
+++ triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        if (n == 0) return 0;
+         
+        for (int i = 1; i < n;++i) {
+            for (int j = 0;j < triangle[i].size();++j) {
+                if (j == 0) triangle[i][j] += triangle[i-1][j];
+                else {
+                    if (i > j) triangle[i][j] += min(triangle[i-1][j-1],triangle[i-1][j]);
+                    else triangle[i][j] += triangle[i-1][j-1];
+                }
+            }
+        }
+        return *min_element(triangle[n-1].begin(),triangle[n-1].end());
+    }
+};
