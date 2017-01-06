5195ecffa3a91e3ffabad6ed2a825563e1599ea4
Qingfei.You <qingfeiyou@gmail.com>
2017-01-06 22:06:23 +0100

    feat(cpp): add solution for range sum query 2d - immutable

---
+++ range_sum_query_2d_immutable.cpp
@@ -0,0 +1,26 @@
+class NumMatrix {
+public:
+    NumMatrix(vector<vector<int>> &matrix) {
+        int m = matrix.size(), n = m == 0 ? 0 : matrix[0].size();
+        vector<vector<int>> grid(m+1, vector<int>(n+1, 0));
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                grid[i][j] = grid[i-1][j]+grid[i][j-1]-grid[i-1][j-1]+matrix[i-1][j-1];
+            }
+        }
+        data = grid;
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return data[row2+1][col2+1] - data[row2+1][col1] - data[row1][col2+1] + data[row1][col1];
+    }
+
+private:
+    vector<vector<int>> data;
+};
+
+
+// Your NumMatrix object will be instantiated and called as such:
+// NumMatrix numMatrix(matrix);
+// numMatrix.sumRegion(0, 1, 2, 3);
+// numMatrix.sumRegion(1, 2, 3, 4);
