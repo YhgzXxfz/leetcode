c9fa9682d97c955c5545c71691bcc7bdcc76d6cb
Qingfei.You <qingfeiyou@gmail.com>
2017-01-07 16:03:24 +0100

    refactor(cpp): rename variable for dungeon game

--- dungeon_game.cpp
+++ dungeon_game.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
         int m = dungeon.size(), n = dungeon[0].size();
-        vector<vector<int>> minInitHealth(m, vector<int>(n,0));
-        for (int i = m-1; i >= 0; --i) {
+        int dp[m][n] = {};
+	for (int i = m-1; i >= 0; --i) {
             for (int j = n-1; j >= 0; --j) {
-                if (i == m-1 && j == n-1) {
-                    minInitHealth[i][j] = max(1, 1 - dungeon[i][j]);
-                } else if (i == m-1) {
-                    minInitHealth[i][j] = max(1, minInitHealth[i][j+1] - dungeon[i][j]);
-                } else if (j == n-1) {
-                    minInitHealth[i][j] = max(1, minInitHealth[i+1][j] - dungeon[i][j]);
-                } else {
-                    minInitHealth[i][j] = max(1, min(minInitHealth[i+1][j],minInitHealth[i][j+1]) - dungeon[i][j]);
-                }  
+                if (i == m-1 && j == n-1) dp[i][j] = max(1, 1-dungeon[i][j]);
+                else if (i == m-1) dp[i][j] = max(1, dp[i][j+1]-dungeon[i][j]);
+                else if (j == n-1) dp[i][j] = max(1, dp[i+1][j]-dungeon[i][j]);
+                else dp[i][j] = max(1, min(dp[i+1][j],dp[i][j+1])-dungeon[i][j]);
             }
         }
-        
-        return  minInitHealth[0][0];
+        return dp[0][0];
     }
 };
