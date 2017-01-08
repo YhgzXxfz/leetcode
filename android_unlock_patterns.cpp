3eb9da05b70eb268191e6d76df2759ffc3d89769
Qingfei.You <qingfeiyou@gmail.com>
2017-01-08 18:00:21 +0100

    refactor(cpp): use backtrack for android unlock patterns

--- android_unlock_patterns.cpp
+++ android_unlock_patterns.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     int numberOfPatterns(int m, int n) {
-        return dfs(m, n, 0, 1, 1, 1, 1);
-    }
-
-private:
-    int dfs(int m, int n, int used, int curr_x, int curr_y, int prev_x, int prev_y) {
-        int number = m <= 0;
-        if (!n) return 1;
+        vector<vector<int>> skip(10, vector<int>(10, 0));
+        skip[1][3] = skip[3][1] = 2;
+        skip[1][7] = skip[7][1] = 4;
+        skip[3][9] = skip[9][3] = 6;
+        skip[7][9] = skip[9][7] = 8;
+        skip[1][9] = skip[9][1] = skip[2][8] = skip[8][2] = skip[3][7] = skip[7][3] = skip[4][6] = skip[6][4] = 5;
         
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                int x = prev_x+i, y = prev_y+j, used2 = used | (1 << (i*3 + j));
-                if (used2 > used && (x&1 || y&1 || used2 & (1 << (x/2*3 + y/2))))
-                    number += dfs(m-1, n-1, used2, prev_x, prev_y, i, j);
-            }
+        vector<int> visited(10, 0);
+        int result = 0;
+        for (int i = m; i <= n; ++i) {
+            result += dfs(visited, skip, 1, i-1)*4;
+            result += dfs(visited, skip, 2, i-1)*4;
+            result += dfs(visited, skip, 5, i-1);
         }
-        return number;
+        return result;
+    }
+    
+private:
+    int dfs(vector<int>& visited, vector<vector<int>>& skip, int curr, int remain) {
+        if (remain < 0) return 0;
+        if (remain == 0) return 1;
+        
+        visited[curr] = 1;
+        int result = 0;
+        for (int i = 1; i <= 9; ++i) {
+            if (!visited[i] && (skip[curr][i] == 0 || visited[skip[curr][i]])) result += dfs(visited, skip, i, remain-1);
+        }
+        visited[curr] = 0;
+        return result;
     }
 };
