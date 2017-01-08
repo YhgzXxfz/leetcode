1231ae84a6a38126d88357e5efcfbd68c6d92634
Qingfei.You <qingfeiyou@gmail.com>
2017-01-08 16:11:22 +0100

    refactor(cpp): adapt solution to new function signature for word break ii

--- word_break_ii.cpp
+++ word_break_ii.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    vector<string> wordBreak(string s, unordered_set<string>& wordDict) {
-            if (mp.find(s) != mp.end()) return mp[s];
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        if (mp.find(s) != mp.end()) return mp[s];
 
-            // check if need to go further
-            vector<string> result; 
-            bool found = false;
-            for (int i = s.size()-1; i >= 0; --i) {
-                if (wordDict.find(s.substr(i)) != wordDict.end()) {
-                    found = true;
-                    break;
-                } 
+	unordered_set<string> st(wordDict.begin(), wordDict.end());
+        // check if need to go further
+        vector<string> result; 
+        bool found = false;
+        for (int i = s.size()-1; i >= 0; --i) {
+            if (st.count(s.substr(i))) {
+                found = true;
+                break;
             }
-            if (!found) return result;
+        }
+        if (!found) return result;
 
-            // go further 
-            if (wordDict.find(s) != wordDict.end()) result.emplace_back(s);
-            for (int i = s.size()-2; i >= 0; --i) {
-                if (wordDict.find(s.substr(0, i+1)) != wordDict.end()) {
-                    vector<string> rest = wordBreak(s.substr(i+1), wordDict);
-                    for (auto temp : rest) {
-                        result.emplace_back(s.substr(0, i+1) + " " + temp);
-                    }
-                } 
-            }
+        // go further 
+        if (st.count(s)) result.emplace_back(s);
+        for (int i = s.size()-2; i >= 0; --i) {
+            if (st.count(s.substr(0, i+1))) {
+                vector<string> rest = wordBreak(s.substr(i+1), wordDict);
+                for (auto temp : rest) {
+                    result.emplace_back(s.substr(0, i+1) + " " + temp);
+                }
+            } 
+        }
 
-            mp[s] = result;
-            return result;
+        mp[s] = result;
+        return result;
     }
 
 private:
