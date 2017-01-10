e67a741c66280992fcdab1b788f3cfe2df61a347
Qingfei.You <qingfeiyou@gmail.com>
2017-01-10 23:09:47 +0100

    refactor(cpp): make string length constant for valid number

--- valid_number.cpp
+++ valid_number.cpp
@@ -3,11 +3,12 @@ public:
     bool isNumber(string s) {
         if (s.empty()) return false;
         
-        int len = s.size(), i = 0;
+        const int len = s.size();
+	int i = 0;
         while (i < len && s[i] == ' ') i++;
-        if (i == len) return false;
+        if (len == i) return false;
         if (s[i] == '+' || s[i] == '-') i++;
-        if (i == len) return false;
+        if (len == i) return false;
         
         int numbers = 0, points = 0;
         while (i < len && (s[i] == '.' || s[i] >= '0' && s[i] <= '9')) {
@@ -16,24 +17,24 @@ public:
             i++;
         }
         if (points > 1 || numbers < 1) return false;
-        if (i == len) return true;
+        if (len == i) return true;
         
         if (s[i] == 'e') {
             i++;
-            if (i == len) return false;
+            if (len == i) return false;
             
             if (s[i] == '+' || s[i] == '-') i++;
-            if (i == len) return false;
+            if (len == i) return false;
             numbers = 0;
             while (i < len && s[i] >= '0' && s[i] <= '9') {
                 numbers++;
                 i++;
             }
             if (numbers < 1) return false;
-            if (i == len) return true;
+            if (len == i) return true;
         }
         
         while (i < len && s[i] == ' ') i++;
-        return i == len;
+        return len == i;
     }
 };
