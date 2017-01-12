34e2b9a9c020b01b2491dd7c51d811b6b7bfbacf
Qingfei.You <qingfeiyou@gmail.com>
2017-01-12 20:25:13 +0100

    refactor(cpp): add straight-forward solution for repeated substring pattern

--- repeated_substring_pattern.cpp
+++ repeated_substring_pattern.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    // substring 
+    bool repeatedSubstringPattern(string str) {
+        int len = str.size();
+        bool flag;
+        for (int i = len/2; i >= 1; --i) {
+            if (len%i) continue;
+            
+            flag = true;
+            string sample = str.substr(0,i);
+            for (int j = i; j < len; j += i) {
+                if (str.substr(j,i) != sample) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) return true;
+        }
+        return false;
+    }
+
+    // dp
+    /*
     bool repeatedSubstringPattern(string str) {
         int i = 1, j = 0, len = str.size();
         int dp[len+1] = {};
@@ -10,4 +32,5 @@ public:
         }
         return dp[len] && dp[len] % (len-dp[len]) == 0;
     }
+    */
 };
