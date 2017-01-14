1768e62975f7dd81313cf5d979543ddbb78bc3fc
Qingfei.You <qingfeiyou@gmail.com>
2017-01-14 13:01:56 +0100

    refactor(cpp): use set for third maximum number

--- third_maximum_number.cpp
+++ third_maximum_number.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        unordered_set<int> st;
-        for (auto& num : nums) {
+        set<int> st;
+	for (auto& num : nums) {
             st.insert(num);
+            if (st.size() > 3) st.erase(st.begin());
         }
         
-        int len = st.size();
-        if (len == 1) return nums[0];
-        else if (len == 2) return *max_element(nums.begin(), nums.end());
-        
-        priority_queue<int, vector<int>, greater<int> > pq;
-        for (auto& distinct_num : st) {
-            pq.push(distinct_num);
-            if (pq.size() > 3) pq.pop();
-        }
-        
-        return pq.top();
+        return st.size() == 3 ? *st.begin() : *st.rbegin();
     }
 };
