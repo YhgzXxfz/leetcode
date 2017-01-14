be18ab6dfdbca22722b56c233c76e29dfe3ac476
Qingfei.You <qingfeiyou@gmail.com>
2017-01-14 19:21:42 +0100

    fix(cpp): deal with integer limit for missing ranges

--- missing_ranges.cpp
+++ missing_ranges.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
-        int prev = lower-1;
+        long long prev = (long long)lower-1;
         vector<string> result;
         for (int i = 0; i <= nums.size(); ++i) {
-            int curr = nums.size() == i ? upper+1 : nums[i];
-            // use curr >= prev+2 instead of curr-prev >= 2 for integer limit problem
-            if (curr >= prev+2) result.emplace_back(getRange(prev+1, curr-1));
+            long long curr = nums.size() == i ? (long long)upper+1 : nums[i];
+            if (curr-prev >= 2) result.emplace_back(getRange(prev+1, curr-1));
             
             prev = curr;
         }
