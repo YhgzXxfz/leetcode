b9a2012b64acb9ad75bb625c666d8af420cd0d72
Qingfei.You <qingfeiyou@gmail.com>
2017-01-14 16:12:55 +0100

    refactor(cpp): simplify solution for two sum ii - input array is sorted

--- two_sum_ii_input_array_is_sorted.cpp
+++ two_sum_ii_input_array_is_sorted.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int> result;
         int left = 0, right = numbers.size()-1;
         while (left < right) {
             int sum = numbers[left] + numbers[right];
             if (sum == target) {
-                result.emplace_back(left+1);
-                result.emplace_back(right+1);
-                return result;
+                return vector<int>({left+1, right+1});
             } else if (sum < target) {
                 left++;
             } else {
                 right--;
             }
         }
-        return result;
+        return vector<int>();
     }
 };
