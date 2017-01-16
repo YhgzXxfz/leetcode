30f4092ba9a8aa75883d974cad55927b1df613e2
Qingfei.You <qingfeiyou@gmail.com>
2017-01-16 22:10:45 +0100

    refactor(cpp): use emplace_back of vector for gray code

--- gray_code.cpp
+++ gray_code.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> grayCode(int n) {
         vector<int> result;
         for (int i = 0;i < 1<<n; ++i) {
-            result.push_back(i>>1 ^ i);
+            result.emplace_back(i>>1 ^ i);
         }
         return result;
     }
