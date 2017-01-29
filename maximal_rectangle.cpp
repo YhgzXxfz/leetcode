708bd769e117466c61baa6f5960677a0b8060afc
Qingfei.You <qingfeiyou@gmail.com>
2017-01-29 20:40:46 +0100

    refactor(cpp): add another solution for maximal rectangle

--- maximal_rectangle.cpp
+++ maximal_rectangle.cpp
@@ -1,31 +1,60 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.empty()) return 0;
+        
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> heights(n+1, 0);
+        int area = 0;
+        
+        for (int i = 0; i < m; ++i) {
+            stack<int> s;
+            for (int j = 0; j <= n; ++j) {
+                if (j < n) heights[j] = matrix[i][j] == '1' ? 1+heights[j] : 0;
+                    
+                if (s.empty() || heights[j] >= heights[s.top()]) s.push(j);
+                else {
+                    while(!s.empty() && heights[j] < heights[s.top()]){
+                        int tp = s.top();
+                        s.pop();
+                        area = max(area, heights[tp] * (s.empty() ? j : (j-s.top()-1)));
+                    }
+                    s.push(j);
+                }
+            }
+        }
+        return area;
+    }
+};
+// dp
+/*
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
         if(matrix.empty()) return 0;
-        const int m = matrix.size();
-        const int n = matrix[0].size();
-        int left[n], right[n], height[n];
-        fill_n(left,n,0); fill_n(right,n,n); fill_n(height,n,0);
+        
+        const int m = matrix.size(), n = matrix[0].size();
+        vector<int> left(n,0), right(n,n), height(n,0);
         int max_area = 0;
         for (int i = 0; i < m; i++) {
-            int cur_left = 0, cur_right = n; 
+            int curr_left = 0, curr_right = n; 
             for (int j = 0; j < n; j++) { // compute height (can do this from either side)
                 if (matrix[i][j] == '1') height[j]++; 
                 else height[j] = 0;
             }
             for (int j = 0; j < n; j++) { // compute left (from left to right)
-                if (matrix[i][j] == '1') left[j] = max(left[j],cur_left);
+                if (matrix[i][j] == '1') left[j] = max(left[j],curr_left);
                 else {
                     left[j] = 0; 
-                    cur_left = j+1;
+                    curr_left = j+1;
                 }
             }
             // compute right (from right to left)
             for (int j = n-1; j >= 0; j--) {
-                if (matrix[i][j] == '1') right[j] = min(right[j],cur_right);
+                if (matrix[i][j] == '1') right[j] = min(right[j],curr_right);
                 else {
                     right[j] = n; 
-                    cur_right = j;
+                    curr_right = j;
                 }    
             }
             // compute the area of rectangle (can do this from either side)
@@ -35,3 +64,4 @@ public:
         return max_area;
     }
 };
+*/
