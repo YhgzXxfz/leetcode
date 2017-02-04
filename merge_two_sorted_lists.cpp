f022f2ef202d178977f39a935cd10d44ac0226c3
Qingfei.You <qingfeiyou@gmail.com>
2017-02-04 22:24:00 +0100

    feat(cpp): add iterative solution for merge two sorted lists

--- merge_two_sorted_lists.cpp
+++ merge_two_sorted_lists.cpp
@@ -6,6 +6,32 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// iterative
+class Solution {
+public:
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		if (!l1) return l2;
+		if (!l2) return l1;
+
+		ListNode fakeHead(0), *curr = &fakeHead;
+		while (l1 && l2) {
+			if (l1->val < l2->val) {
+				curr->next = l1;
+				l1 = l1->next;
+			} else {
+				curr->next = l2;
+				l2 = l2->next;
+			}
+			curr = curr->next;
+		}
+		if (l1) curr->next = l1;
+		if (l2) curr->next = l2;
+		return fakeHead.next;
+	}
+};
+
+
+// recursive
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
