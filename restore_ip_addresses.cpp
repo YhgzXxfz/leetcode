9fcc14657c9dcccf1f41ed08a7a1fc857a05074f
Qingfei.You <qingfeiyou@gmail.com>
2017-02-05 21:19:05 +0100

    feat(cpp): add an easier solution for restore ip addresses

--- restore_ip_addresses.cpp
+++ restore_ip_addresses.cpp
@@ -3,30 +3,22 @@ public:
     vector<string> restoreIpAddresses(string s) {
         vector<string> result;
         string ip;
-        dfs(result, 0, 0, s, ip);
-        return result;
-    }
-    
-private:
-    void dfs(vector<string>& result, int start, int step, string s, string ip) {
-        if (start == s.size() && step == 4) {
-            ip.resize(ip.size() -1);
-            result.push_back(ip);
-            return;
-        }
         
-        if (s.size() - start > (4-step)*3) return;
-        if (s.size() - start < 4-step) return;
-        
-        int num = 0;
-        for (int i = start; i < start+3; ++i) {
-            num = num*10 + (s[i]-'0');
-            if (num <= 255) {
-                ip += s[i];
-                dfs(result, i+1, step+1, s, ip+'.');
+        for (int a = 1; a <= 3; ++a) {
+            for (int b = 1; b <= 3; ++b) {
+                for (int c = 1; c <= 3; ++c) {
+                    for (int d = 1; d <= 3; ++d) {
+                        if (a+b+c+d == s.size()) {
+                            int A = stoi(s.substr(0, a)), B = stoi(s.substr(a, b)), C = stoi(s.substr(a+b, c)), D = stoi(s.substr(a+b+c, d));
+                            if (A <= 255 && B <= 255 && C <= 255 && D <= 255) {
+                                ip = to_string(A) + "." + to_string(B) + "." + to_string(C) + "." + to_string(D);
+                                if (ip.size() == s.size()+3) result.emplace_back(ip);
+                            }
+                        }
+                    }
+                }
             }
-            
-            if (num == 0) break;
         }
+        return result;
     }
 };
