b1e0aff7ac44b4d44669c87bf73820f092999911
Qingfei.You <qingfeiyou@gmail.com>
2017-02-07 23:19:33 +0100

    feat(cpp): add another solution for distince subsequences

--- distinct_subsequences.cpp
+++ distinct_subsequences.cpp
@@ -14,3 +14,23 @@ public:
         return dp[lenT];
     }
 };
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int lenS = s.size(), lenT = t.size();
+        
+        vector<vector<int>> dp(lenS+1, vector<int>(lenT+1,0));
+        for (int i = 0; i <= lenS; ++i) {
+            dp[i][0] = 1;
+        }
+        
+        for (int i = 1; i <= lenS; ++i) {
+            for (int j = 1; j <= lenT; ++j) {
+                if (s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[lenS][lenT];
+    }
+};
