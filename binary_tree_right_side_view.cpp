6ba2a1913b8cf5e902f9af72b3eac4d8bbd4c630
Qingfei.You <qingfeiyou@gmail.com>
2017-02-09 00:48:56 +0100

    style(cpp): make help function private for binary tree right side view

--- binary_tree_right_side_view.cpp
+++ binary_tree_right_side_view.cpp
@@ -14,10 +14,12 @@ public:
         dfs(root, 0, result);
         return result;
     }
-    
+
+private:    
     void dfs(TreeNode* root, int level, vector<int>& result) {
         if (!root) return;
-        if (level >= result.size()) result.push_back(root->val);
+        
+	if (level >= result.size()) result.push_back(root->val);
         dfs(root->right, level+1, result);
         dfs(root->left, level+1, result);
     }
