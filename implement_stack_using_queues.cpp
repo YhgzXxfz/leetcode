ace3c9e69887f105825b25164f4d538d137eb0b7
Qingfei.You <qingfeiyou@gmail.com>
2017-02-11 00:23:18 +0100

    chore(cpp): adapt solution to new signature for implement stack using queues

--- implement_stack_using_queues.cpp
+++ implement_stack_using_queues.cpp
@@ -1,29 +1,46 @@
-class Stack {
+class MyStack {
 public:
-    // Push element x onto stack.
+    /** Initialize your data structure here. */
+    MyStack() {
+        
+    }
+    
+    /** Push element x onto stack. */
     void push(int x) {
+        int len = q.size();
         q.push(x);
-        for (int i = 0; i < q.size()-1; ++i) {
+        for (int i = 0; i < len; ++i) {
             q.push(q.front());
             q.pop();
         }
     }
-
-    // Removes the element on top of the stack.
-    void pop() {
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int elem = q.front();
         q.pop();
+        return elem;
     }
-
-    // Get the top element.
+    
+    /** Get the top element. */
     int top() {
         return q.front();
     }
-
-    // Return whether the stack is empty.
+    
+    /** Returns whether the stack is empty. */
     bool empty() {
         return q.empty();
     }
-
+    
 private:
     queue<int> q;
 };
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * bool param_4 = obj.empty();
+ */
