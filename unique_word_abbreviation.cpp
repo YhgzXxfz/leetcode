ecf69e143d6a1ae3977d2d70dc3ed7caa66f972a
Qingfei.You <qingfeiyou@gmail.com>
2017-02-11 16:14:14 +0100

    refactor(cpp): add function to generate abbreviation for unique word abbreviation

--- unique_word_abbreviation.cpp
+++ unique_word_abbreviation.cpp
@@ -1,26 +1,29 @@
 class ValidWordAbbr {
 public:
-    ValidWordAbbr(vector<string> &dictionary) {
-        for (string word : dictionary) {
-            int len = word.size();
-            string abbrev = len <= 2 ? word : word[0]+to_string(len-2)+word[len-1];
+    ValidWordAbbr(vector<string> dictionary) {
+        for (auto word : dictionary) {
+            string abbrev = generateAbbreviation(word);
             mp[abbrev].insert(word);
         }
     }
-
+    
     bool isUnique(string word) {
-        int len = word.size();
-        string abbrev = len <= 2 ? word : word[0]+to_string(len-2)+word[len-1];
+        string abbrev = generateAbbreviation(word);
         if (mp.find(abbrev) == mp.end()) return true;
         return mp[abbrev].count(word) == mp[abbrev].size();
     }
 
 private:
+    string generateAbbreviation(string word) {
+        int len = word.size();
+        return len <= 2 ? word : word[0]+to_string(len-2)+word[len-1];
+    }
+    
     unordered_map<string, unordered_set<string>> mp;
 };
 
-
-// Your ValidWordAbbr object will be instantiated and called as such:
-// ValidWordAbbr vwa(dictionary);
-// vwa.isUnique("hello");
-// vwa.isUnique("anotherWord");
+/**
+ * Your ValidWordAbbr object will be instantiated and called as such:
+ * ValidWordAbbr obj = new ValidWordAbbr(dictionary);
+ * bool param_1 = obj.isUnique(word);
+ */
