8762ae59c6b5044a1cf4d7613c60154fecca54c0
Qingfei.You <qingfeiyou@gmail.com>
2017-02-11 13:53:17 +0100

    feat(cpp): add another solution for missing number

--- missing_number.cpp
+++ missing_number.cpp
@@ -5,3 +5,15 @@ public:
         return (len*(len+1)/2 - accumulate(nums.begin(), nums.end(), 0));
     }
 };
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+    	int result = nums.size();
+	for (int i = 0; i < nums.size(); ++i) {
+		result ^= i;
+		result ^= nums[i];
+	}
+	return result;
+    }
+};
