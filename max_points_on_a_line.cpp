40c919807235b0eca7b4a5cc82f48d94f1c710c3
Qingfei.You <qingfeiyou@gmail.com>
2017-03-25 21:30:52 +0800

    fix(cpp): use pair of integer instead of double slope as key of lines for max points on a line

--- max_points_on_a_line.cpp
+++ max_points_on_a_line.cpp
@@ -9,30 +9,38 @@
  */
 class Solution {
 public:
-    int maxPoints(vector<Point>& points) {
-        if (points.empty()) return 0;
-        
-        int max_point = 0;
-        for (int i = 0; i < points.size(); i++) {
-            int same_point = 1;
-            unordered_map<double, int> mp;
-            for (int j = i + 1; j < points.size(); j++) {
-                if (points[i].x == points[j].x && points[i].y == points[j].y) {
-                    same_point++;
-                } else if (points[i].x == points[j].x) {
-                    mp[INT_MAX]++;
-                } else {
-                    double slope = double(points[i].y - points[j].y) / double(points[i].x - points[j].x);
-                    mp[slope]++;
-                }
-            }
-            int curr_max = 0;
-            for (auto& entry : mp){
-                curr_max = max(curr_max, entry.second);
-            }
-            curr_max += same_point;
-            max_point = max(max_point, curr_max);
-        }
-        return max_point;
-    }
+    	int maxPoints(vector<Point>& points) {
+    		int len = points.size();
+		if (len < 2) return len;
+
+		int total_max = INT_MIN;
+		for (int i = 0; i < len; ++i) {
+			auto pi = points[i];
+			map<pair<int, int>, int> mp;
+			int curr_max = 0, same_points = 0, verticals = 0;
+			for (int j = i+1; j < len; ++j) {
+				if (pi.x == pj.x && pi.y == pj.y) same_points++;
+				else if (pi.x == pj.x) verticals++;
+				else {
+					auto pj = points[j];
+					int a = pi.x-pj.x, b = pi.y-pj.y;
+					int gcd = computeGcd(int a, int b);
+					a /= gcd; b /= gcd;
+					mp[make_pair(a,b)]++;
+				}
+			}
+
+			for (auto entry : mp) {
+				curr_max = max(curr_max, entry.second);
+			}
+			curr_max = max(curr_max, verticals);
+			curr_max += same_points+1; // +1 means points[i] itself
+			total_max = max(total_max, curr_max);
+		}
+	}
+
+private:
+	int computeGcd(int a, int b) {
+		return b == 0 ? a : computeGcd(b, a%b);
+	}
 };
