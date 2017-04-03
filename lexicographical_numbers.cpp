4457aa2cc01b5725ceb2ad2bc617b0ee8120ed5a
Qingfei.You <qingfeiyou@gmail.com>
2017-04-03 22:11:52 +0800

    refactor(cpp): use simpler solution for lexicographical numbers

--- lexicographical_numbers.cpp
+++ lexicographical_numbers.cpp
@@ -2,24 +2,20 @@ class Solution {
 public:
     vector<int> lexicalOrder(int n) {
         vector<int> result;
-        int i = 1, j, k;
-        for (;;) {
-            for (k = 0; i*pow(10, k) <= n; ++k) {
-                result.emplace_back(i*pow(10, k));
-            }
-            
-            for (j = result.back()+1; j <=n && (j%10) != 0; ++j) {
-                result.emplace_back(j);
-            }
-            
-            if (j%10 == 0) j--;
-            else j /= 10;
-            
-            while (j%10 == 9) j /= 10;
-            i = j+1;
-            
-            if (result.size() >= n) break;
-        }
-        return result;
+	int curr = 1;
+	for (int i = 1; i <= n; ++i) {
+		result.emplace_back(curr);
+		if (curr *10 <= n) {
+			curr *= 10;
+		} else if (curr %10 != 9 && curr+1 <= n) {
+			curr++;
+		} else {
+			while ((curr/10) %10 == 9) {
+				curr /= 10;
+			}
+			curr = curr/10 +1;
+		}
+	}
+	return result;
     }
 };
