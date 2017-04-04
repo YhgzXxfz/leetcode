4acfb9dc2110e88b16bb252f2e2613f48fef722a
Qingfei.You <qingfeiyou@gmail.com>
2017-04-04 14:13:01 +0800

    feat(cpp): add solution for valid word square

--- valid_word_square.cpp
+++ valid_word_square.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     bool validWordSquare(vector<string>& words) {
-        if (words.empty()) return true;
-        
-        int m = words.size();
-        if (words[0].size() != m) return false;
-        for (int i = 0; i < m; ++i) {
-            int n = words[i].size();
-            if (n > m) return false;
-            
-            string temp;
-            for (int j = 0; j < n; ++j) {
-                if (words[j].size() < i+1) return false;
-                temp += words[j][i];
-            }
-            if (temp != words[i]) return false;
-        }
-        return true;
+        for (int i = 0; i < words.size(); ++i) {
+		for (int j = 0; j < words[i].size(); ++j) {
+			if (j >= words.size() || words[j].size() <= i || words[j][i] != words[i][j])
+				return false;
+		}
+	}
+	return true;
     }
 };
