9ad3cbcaee177995e6d85df4a9cd2060cf6b2dcd
Qingfei.You <qingfeiyou@gmail.com>
2017-04-05 17:56:18 +0800

    feat(java): add random fixed-length solution for encode and decode tinyurl

--- encode_and_decode_tinyurl.java
+++ encode_and_decode_tinyurl.java
@@ -1,4 +1,39 @@
 public class Codec {
+    String alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    HashMap<String, String> map = new HashMap<>();
+    Random rand = new Random();
+    String key = getRand();
+
+    public String getRand() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            sb.append(alphabet.charAt(rand.nextInt(62)));
+        }
+        return sb.toString();
+    }
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        while (map.containsKey(key)) {
+            key = getRand();
+        }
+        map.put(key, longUrl);
+        return "http://tinyurl.com/" + key;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(shortUrl.replace("http://tinyurl.com/", ""));
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
+
+
+
+public class Codec {
     HashMap<String,String> tinyUrlMap = new HashMap<>();
     HashMap<String,String> longUrlMap = new HashMap<>();
     String tinyPrefix = "https://tinyurl.com/";
