ef8ddc7d7cc5e26bf4065e38bfc3a9a004090c55
Qingfei.You <qingfeiyou@gmail.com>
2017-04-06 15:44:17 +0800

    feat(cpp): add right to left traversal solution for find bottom left tree value

--- find_bottom_left_tree_value.cpp
+++ find_bottom_left_tree_value.cpp
@@ -10,6 +10,34 @@
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        TreeNode* curr = nullptr;
+        while (!q.empty()) {
+            curr = q.front();
+            q.pop();
+            if (curr->right) q.push(curr->right);
+            if (curr->left) q.push(curr->left);
+        }
+        return curr->val;
+    }
+};
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
         vector<vector<int>> results;
         traverseByLevel(root, results, 0);
         return *(results.back().begin());
