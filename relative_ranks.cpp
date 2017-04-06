46eb6f5971442427e02a4e38c759c1eeeb1f2680
Qingfei.You <qingfeiyou@gmail.com>
2017-04-06 11:43:28 +0800

    feat(cpp): add priority queue solution for relative ranks

--- relative_ranks.cpp
+++ relative_ranks.cpp
@@ -1,3 +1,32 @@
+// priority queue (insert sort)
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& nums) {
+        int len = nums.size();
+        priority_queue<pair<int,int> > pq;
+        for (int i = 0; i < len; ++i) {
+            pq.push(make_pair(nums[i],i));
+        }
+        
+        vector<string> result(len);
+        int rank = 1;
+        for (int i = 0; i < len; ++i) {
+            result[pq.top().second] = (rank <= 3) ? getTopThree(rank) : to_string(rank);
+            rank++;
+            pq.pop();
+        }
+        return result;
+    }
+
+private:
+    string getTopThree(int rank) {
+        return 1 == rank ? "Gold Medal" : 2 == rank ? "Silver Medal" : "Bronze Medal";
+    }
+};
+
+
+
+// sort
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& nums) {
