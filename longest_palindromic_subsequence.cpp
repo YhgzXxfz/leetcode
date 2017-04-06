d3784f54e25e971b44ec82f8d822eed9280c7e94
Qingfei.You <qingfeiyou@gmail.com>
2017-04-06 17:45:53 +0800

    feat(cpp): add recursive solution for longest palindromic subsequence

--- longest_palindromic_subsequence.cpp
+++ longest_palindromic_subsequence.cpp
@@ -14,3 +14,29 @@ public:
         return dp[0][len-1];
     }
 };
+
+
+
+
+// memory limit exceeded
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int len = s.size();
+        vector<vector<int>> cache(len, vector<int>(len));
+        return dfs(s, 0, len-1, cache);
+    }
+    
+private:
+    int dfs(string s, int left, int right, vector<vector<int>>& cache) {
+        if (cache[left][right]) return cache[left][right];
+        
+        if (left > right) return 0;
+        if (left == right) return 1;
+        
+        if (s[left] == s[right]) cache[left][right] = dfs(s, left+1, right-1, cache) +2;
+        else cache[left][right] = max(dfs(s, left+1, right, cache), dfs(s, left, right-1, cache));
+        
+        return cache[left][right];
+    }
+};
