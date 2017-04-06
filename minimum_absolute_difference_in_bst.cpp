2071de61ddb52523f09aa7c3177be47637c6bda3
Qingfei.You <qingfeiyou@gmail.com>
2017-04-06 22:22:12 +0800

    feat(cpp): add O(1) space complexity solution for minimum absolute difference in bst

--- minimum_absolute_difference_in_bst.cpp
+++ minimum_absolute_difference_in_bst.cpp
@@ -10,6 +10,37 @@
 class Solution {
 public:
     int getMinimumDifference(TreeNode* root) {
+        dfs(root);
+        return min_diff;
+    }
+
+private:
+    int prev = -1, min_diff = INT_MAX;
+    
+    void dfs(TreeNode* curr) {
+        if (!curr) return ;
+        
+        dfs(curr->left);
+        if (prev >= 0) min_diff = min(min_diff, abs(curr->val -prev));
+        prev = curr->val;
+        dfs(curr->right);
+    }
+};
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getMinimumDifference(TreeNode* root) {
         vector<int> inorder;
         dfs(root, inorder);
         int min_diff = INT_MAX;
