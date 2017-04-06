cece50e9c5d14156ea0f307dcd354519ed45ee25
Qingfei.You <qingfeiyou@gmail.com>
2017-04-06 10:42:42 +0800

    feat(cpp): add solution using stack for next greater element ii

--- next_greater_element_ii.cpp
+++ next_greater_element_ii.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
+        int len = nums.size();
+        stack<int> s;
+        vector<int> result(len, -1);
+        
+        for (int i = 0; i < 2*len; ++i) {
+            int num = nums[i%len];
+            while (!s.empty() && nums[s.top()] < num) {
+                result[s.top()] = num;
+                s.pop();
+            }
+            if (i < len) s.push(i);
+        }
+        return result;
+    }
+};
+
+
+// brute force
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
         vector<int> result, doubled_nums(nums);
         doubled_nums.insert(doubled_nums.end(), doubled_nums.begin(), doubled_nums.end());
         
