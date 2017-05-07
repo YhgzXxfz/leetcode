73bd54d74d0e18fba233af3d889d72e31373aea9
Qingfei.You <qingfeiyou@gmail.com>
2017-05-07 11:03:40 +0200

    refactor(cpp): reduce memory complexity for find largest value in each tree row

--- find_largest_value_in_each_tree_row.cpp
+++ find_largest_value_in_each_tree_row.cpp
@@ -7,6 +7,29 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// 1
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        vector<int> result;
+        dfs(root, result, 0);
+        return result;
+    }
+
+private:
+    void dfs(TreeNode* root, vector<int>& result, int level) {
+        if (!root) return ;
+        
+        if (level >= result.size()) result.push_back(root->val);
+        else result[level] = (max(result[level], root->val));
+        
+        dfs(root->left, result, level+1);
+        dfs(root->right, result, level+1);
+    }
+};
+
+
+// 2
 class Solution {
 public:
     vector<int> largestValues(TreeNode* root) {
