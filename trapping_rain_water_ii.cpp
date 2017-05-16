ce81bbe82029b549c062876f4bd2a1e3f1baddc3
Qingfei.You <qingfeiyou@gmail.com>
2017-05-16 21:46:25 +0200

    refactor(cpp): simplify solution for trapping rain water ii

--- trapping_rain_water_ii.cpp
+++ trapping_rain_water_ii.cpp
@@ -5,7 +5,7 @@ public:
         
         int m = heightMap.size(), n = heightMap[0].size();
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        visited.resize(m, vector<int>(n, 0));
+        vector<vector<int>> visited(m, vector<int>(n, 0));
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (i == 0 || i == m-1 || j == 0 || j == n-1) {
@@ -15,26 +15,23 @@ public:
             }
         }
         
-        int result = 0;
+        int result = 0, max_height = INT_MIN;
         vector<vector<int>> directions{{0,1}, {1,0}, {0,-1}, {-1,0}};
         while (!pq.empty()) {
-            auto val = pq.top();
+            auto curr = pq.top();
             pq.pop();
-            int height = val.first, x = val.second/n, y = val.second%n;
-            max_height = max(max_height, height);
-            for (auto& dir : directions) {
-                int new_x = x+dir[0], new_y = y+dir[1];
-                if (new_x < 0 || new_x >= m || new_y < 0 || new_y >= n || visited[new_x][new_y]) continue;
+            int i = curr.second/n, j = curr.second%n;
+            max_height = max(max_height, curr.first);
+            for (auto& d : directions) {
+                int x = i+d[0], y = j+d[1];
+                if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y]) continue;
                 
-                if (max_height > heightMap[new_x][new_y]) result += (max_height-heightMap[new_x][new_y]);
-                pq.push(make_pair(heightMap[new_x][new_y], new_x*n+new_y));
-                visited[new_x][new_y] = 1;
+		int h = heightMap[x][y];
+                if (max_height > h) result += (max_height-h);
+                pq.push(make_pair(h, x*n+y));
+                visited[x][y] = 1;
             }
         }
         return result;
     }
-
-private:
-    int max_height = INT_MIN;
-    vector<vector<int>> visited;
 };
