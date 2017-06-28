3f46a80fee692c8fd6517080ae4b7b677ba57bf0
Qingfei.You <qingfeiyou@gmail.com>
2017-06-28 21:56:53 +0200

    feat(cpp): add bucket sort solution for contains duplicate iii

--- contains_duplicate_iii.cpp
+++ contains_duplicate_iii.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        set<int> window;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (i > k) window.erase(nums[i-k-1]); // keep the set contains nums i j at most k
-            // -t <= x - nums[i] <= t;
-            auto pos = window.lower_bound(nums[i] - t); // x >= nums[i] - t
-            if (pos != window.end() && *pos - nums[i] <= t) // x <= nums[i] + t
-                   return true;
-            window.insert(nums[i]);
-        }
-        return false;
-    }
+    	bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+    		if (t < 0) return false;
+
+		unordered_map<long, long> mp;
+		long w = (long)t +1;
+		for (int i = 0; i < nums.size(); ++i) {
+			long id = getId(nums[i], w);
+
+			if (mp.count(id)) return true;
+			if (mp.count(id-1) && abs(mp[id-1]-nums[i]) <= t) return true;
+			if (mp.count[id+1) && abs(mp[id+1]-nums[i]) <= t) return true;
+
+			mp[id] = (long) nums[i];
+			if (mp.size() > k) mp.erase(getId(nums[i-k], w));
+		}
+		return false;
+	}
+
+private:
+	long getId(int num, int width) {
+		return num < 0 ? (num+1)/width -1 : num/width;
+	}
 };
