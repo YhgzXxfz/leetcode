9de3d0580b1acacf484cfd7e5efecb4d2b576be7
Qingfei.You <qingfeiyou@gmail.com>
2017-07-26 22:45:15 +0200

    feat(cpp): add dp solution for out of boundary paths

--- out_of_boundary_paths.cpp
+++ out_of_boundary_paths.cpp
@@ -1,3 +1,4 @@
+// dfs + cache
 class Solution {
 public:
     int findPaths(int m, int n, int N, int i, int j) {
@@ -17,4 +18,32 @@ private:
     }
     
     int M = 1e9+7;
+};
+
+
+// dp
+class Solution {
+public:
+    int findPaths(int m, int n, int N, int x, int y) {
+        int M = 1e9+7;
+		vector<vector<int>> dp(m, vector<int>(n,0));
+		dp[x][y] = 1;
+		int count = 0;
+		for (int moves = 1; moves <= N; ++moves) {
+			auto temp = dp;
+			for (int i = 0; i < m; ++i) {
+				for (int j = 0; j < n; ++j) {
+					if (i == m - 1) count = (count + dp[i][j]) % M;
+                    if (j == n - 1) count = (count + dp[i][j]) % M;
+                    if (i == 0) count = (count + dp[i][j]) % M;
+                    if (j == 0) count = (count + dp[i][j]) % M;
+
+                    temp[i][j] = (((i > 0 ? dp[i - 1][j] : 0) +(i < m - 1 ? dp[i + 1][j] : 0)) %M + 
+                                ((j > 0 ? dp[i][j - 1] : 0) + (j < n - 1 ? dp[i][j + 1] : 0)) % M) % M;
+				}
+			}
+			dp = temp;
+		}
+		return count;
+    }
 };
