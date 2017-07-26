7483f71f7fb3c9d500893bb92e79f79e308f7444
Qingfei.You <qingfeiyou@gmail.com>
2017-07-26 21:40:40 +0200

    feat(cpp): add stack solution for shortest unsorted continuous subarray

--- shortest_unsorted_continuous_subarray.cpp
+++ shortest_unsorted_continuous_subarray.cpp
@@ -1,3 +1,4 @@
+// 1 sort
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
@@ -12,3 +13,34 @@ public:
         return right > left ? right-left+1 : 0;
     }
 };
+
+
+// 2 stack
+class Solution {
+public:
+	int findUnsortedSubarray(vector<int>& nums) {
+		int len = nums.size();
+		if (len <= 1) return 0;
+
+		stack<int> s;
+		int left = len-1, right = 0;
+		for (int i = 0; i < len; ++i) {
+			while (!s.empty() && nums[i] < nums[s.top()]) {
+				left = min(left, s.top());
+				s.pop();
+			}
+			s.push(i);
+		}
+
+		s = stack<int>();
+		for (int i = len-1; i >= 0; --i) {
+			while (!s.empty() && nums[i] > nums[s.top()]) {
+				right = max(right, s.top());
+				s.pop();
+			}
+			s.push(i);
+		}
+		return right > left ? right-left+1 : 0;
+	}
+};
+
