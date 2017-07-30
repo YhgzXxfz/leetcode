a773b5906845964404f5c3b71bb36c4e5964293e
Qingfei.You <qingfeiyou@gmail.com>
2017-07-30 16:28:47 +0200

    feat(cpp): add dfs solution for friend circles

--- friend_circles.cpp
+++ friend_circles.cpp
@@ -26,3 +26,35 @@ public:
         return count;
     }
 };
+
+
+// dfs
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& matrix) {
+        if (matrix.empty()) return 0;
+        
+        int len = matrix.size(), count = 0;
+        vector<int> visited(len, 0);
+        for (int i = 0; i < len; ++i) {
+            if (visited[i]) continue;
+            
+            dfs(matrix, visited, i);
+            count++;
+        }
+        return count;
+    }
+
+private:
+    void dfs(vector<vector<int>>& matrix, vector<int>& visited, int curr) {
+        for (int i = 0; i < visited.size(); ++i) {
+            if (visited[i]) continue;
+            
+            if (matrix[curr][i]) {
+                visited[curr] = true;
+                dfs(matrix, visited, i);
+            }
+        }
+    }
+};
