65b160a08b55569f80eae075dcc853690a74f978
Qingfei.You <qingfeiyou@gmail.com>
2017-08-06 11:26:50 +0200

    feat(cpp): add solution for maximum distance in arrays

--- maximum_distance_in_arrays.cpp
+++ maximum_distance_in_arrays.cpp
@@ -13,3 +13,19 @@ public:
         return max_dist;
     }
 };
+
+// t-O(N)
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int min_val = arrays[0][0], max_val = arrays[0].back(), max_dist = 0;
+        for (int i = 1; i < arrays.size(); ++i) {
+            int curr_min = arrays[i][0], curr_max = arrays[i].back();
+            max_dist = max(max_dist, max(abs(curr_max - min_val), abs(max_val - curr_min)));
+            
+            min_val = min(min_val, curr_min);
+            max_val = max(max_val, curr_max);
+        }
+        return max_dist;
+    }
+};
