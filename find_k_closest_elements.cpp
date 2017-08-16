d0b3070197a49ffa931f6ce8dd5729b172ccb3c5
Qingfei.You <qingfeiyou@gmail.com>
2017-08-16 21:31:23 +0200

    feat(cpp): add binary search solution for find k closest elements

--- find_k_closest_elements.cpp
+++ find_k_closest_elements.cpp
@@ -1,3 +1,20 @@
+// binary search, t-O(logN + k)
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int index = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
+        int i = index-1, j = index;
+        while (k--) {
+            if (i < 0 || (j < arr.size() && abs(arr[i]-x) > abs(arr[j]-x))) ++j;
+            else --i;
+        }
+        return vector<int>(arr.begin()+i+1, arr.begin()+j);
+    }
+};
+
+
+
+// pq + sort, t-O(N)
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
