af743ac99979cbf3305b9fcae369fc58ffb3cccf
Qingfei.You <qingfeiyou@gmail.com>
2017-08-18 22:37:01 +0200

    feat(cpp): add bfs solution for the maze

--- the_maze.cpp
+++ the_maze.cpp
@@ -1,3 +1,38 @@
+// bfs
+class Solution {
+public:
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        if (start == destination) return true;
+        
+        int m = maze.size(), n = maze[0].size();
+        vector<vector<int>> dirs = {{0,1}, {1,0}, {-1,0}, {0,-1}}, visited(m, vector<int>(n,0));
+        visited[start[0]][start[1]] = 1;
+        queue<pair<int,int>> q;
+        q.push(make_pair(start[0], start[1]));
+        while (!q.empty()) {
+            auto curr = q.front();
+            q.pop();
+            
+            for (auto dir : dirs) {
+                int x = curr.first, y = curr.second;
+                while (x >= 0 && x < m && y >= 0 && y < n && maze[x][y] == 0) {
+                    x += dir[0]; y += dir[1];
+                }
+                x -= dir[0]; y -= dir[1];
+                
+                if (visited[x][y]) continue;
+                visited[x][y] = true;
+                
+                if (destination[0] == x && destination[1] == y) return true;
+                q.push(make_pair(x,y));
+            } 
+        }
+        return false;
+    }
+};
+
+
+// dfs
 class Solution {
 public:
     bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
