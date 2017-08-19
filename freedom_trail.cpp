9adc71450e8300f7df79b244fd2b0d12bdbc8598
Qingfei.You <qingfeiyou@gmail.com>
2017-08-19 13:29:52 +0200

    feat(cpp): add dp solution for freedom trail

--- freedom_trail.cpp
+++ freedom_trail.cpp
@@ -24,3 +24,27 @@ private:
         return cache[p1][p2];
     }
 };
+
+
+// dp
+class Solution {
+public:
+    int findRotateSteps(string ring, string key) {
+        int r = ring.size(), k = key.size();
+        vector<vector<int>> dict(26, vector<int>());
+        for (int i = 0; i < ring.size(); ++i) {
+            dict[ring[i]-'a'].push_back(i);
+        }
+        vector<vector<int>> dp(k+1, vector<int>(r, INT_MAX));
+        dp[k].assign(r, 0);
+        for (int i = k-1; i >= 0; --i) {
+            for (int j = 0; j < r; ++j) {
+                for (auto pos : dict[key[i]-'a']) {
+                    int dist = abs(pos-j);
+                    dp[i][j] = min(dp[i][j], min(dist, r-dist) + dp[i+1][pos]);
+                }
+            }
+        }
+        return dp[0][0]+k;
+    }
+};
