aa1e4560cfbb747637b264e8b168a14a04b02709
Qingfei.You <qingfeiyou@gmail.com>
2017-08-19 13:48:10 +0200

    refactor(cpp): simplify dfs + cache solution for freedom trail

--- freedom_trail.cpp
+++ freedom_trail.cpp
@@ -2,23 +2,24 @@
 class Solution {
 public:
     int findRotateSteps(string ring, string key) {
+	int r = ring.size(), k = key.size();
         vector<vector<int>> dict(26, vector<int>());
         for (int i = 0; i < ring.size(); ++i) {
             dict[ring[i]-'a'].push_back(i);
         }
-        vector<vector<int>> cache(ring.size(), vector<int>(key.size()));
-        return dfs(ring, key, 0, 0, dict, cache);
+        vector<vector<int>> cache(r, vector<int>(k));
+        return dfs(r, key, 0, 0, dict, cache);
     }
 
 private:
-    int dfs(string& ring, string& key, int p1, int p2, vector<vector<int>>& dict, vector<vector<int>>& cache) {
+    int dfs(int r, string& key, int p1, int p2, vector<vector<int>>& dict, vector<vector<int>>& cache) {
         if (p2 == key.size()) return 0;
         if (cache[p1][p2]) return cache[p1][p2];
         
-        int count = INT_MAX, len = ring.size();
+        int count = INT_MAX;
         for (auto pos : dict[key[p2]-'a']) {
             int dist = abs(pos-p1);
-            count = min(count, min(dist, len-dist) + dfs(ring, key, pos, p2+1, dict, cache));
+            count = min(count, min(dist, r-dist) + dfs(r, key, pos, p2+1, dict, cache));
         }
         cache[p1][p2] = count+1;
         return cache[p1][p2];
