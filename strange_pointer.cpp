a82fd92b6ee75304110aa21725d6cce434195d03
Qingfei.You <qingfeiyou@gmail.com>
2017-08-22 23:59:42 +0200

    feat(cpp): add dp solution for strange pointer

--- strange_pointer.cpp
+++ strange_pointer.cpp
@@ -1,3 +1,26 @@
+// dp
+class Solution {
+public:
+    int strangePrinter(string s) {
+        int len = s.size();
+        vector<vector<int>> dp(100, vector<int>(100));
+        for (int i = 0; i < len; ++i) dp[i][i] = 1;
+        
+        for (int i = 1; i < len; ++i) {
+            for (int j = 0; j < len-i; ++j) {
+                dp[j][j+i] = i+1;
+                for (int k = j+1; k <= j+i; ++k) {
+                    int temp = dp[j][k-1] + dp[k][j+i];
+                    if (s[k-1] == s[j+i]) temp--;
+                    dp[j][j+i] = min(dp[j][j+i], temp);
+                }
+            }
+        }
+        return dp[0][len-1];
+    }
+};
+
+// dfs + cache
 class Solution {
 public:
     int strangePrinter(string s) {
