f5dc09e70b433ee821c6d012cf5b0a28c546596d
Qingfei.You <qingfeiyou@gmail.com>
2017-09-10 15:59:10 +0200

    refactor(cpp): simplify solution for longest increasing path in a matrix

--- longest_increasing_path_in_a_matrix.cpp
+++ longest_increasing_path_in_a_matrix.cpp
@@ -2,46 +2,34 @@ class Solution {
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         if (matrix.empty()) return 0;
-        int row = matrix.size(), col = matrix[0].size();
+        int m = matrix.size(), n = matrix[0].size();
         
         int max_path = INT_MIN, len = 1;
-        vector<vector<int>> cache(row, vector<int> (col, 0));
-        for (int i = 0; i < row; ++i) {
-            for (int j = 0; j < col; ++j) {
-                len = LongestIncreasingPath(matrix, i, j, cache);
+        vector<vector<int>> cache(m, vector<int> (n, 0));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                len = dfs(matrix, i, j, cache);
                 max_path = max(max_path, len);
             }
         }
         return max_path;
     }
-    
-    int LongestIncreasingPath(vector<vector<int>>& matrix, int i, int j, vector<vector<int>>& cache) {
-        int row = matrix.size(), col = matrix[0].size();
-        if (i < 0 || i >= row || j < 0 || j >= col) return 0;
+
+private:
+    int dfs(vector<vector<int>>& matrix, int i, int j, vector<vector<int>>& cache) {
         if (cache[i][j] != 0) return cache[i][j];
         
-        int curr = matrix[i][j], max_path = 1;
-        if (i > 0 && matrix[i-1][j] > curr) {
-            int len = 1+LongestIncreasingPath(matrix, i-1, j, cache);
-            max_path = max(max_path, len);
+        int m = matrix.size(), n = matrix[0].size();
+        int max_len = 1;
+        for (auto dir : dirs) {
+            int x = i+dir[0], y = j+dir[1];
+            if (x >= 0 && x < m && y >= 0 && y < n && matrix[x][y] > matrix[i][j]) {
+                max_len = max(max_len, 1+dfs(matrix, x, y, cache));
+            }
         }
-        
-        if (i < row-1 && matrix[i+1][j] > curr) {
-            int len = 1+LongestIncreasingPath(matrix, i+1, j, cache);
-            max_path = max(max_path, len);
-        }
-        
-        if (j > 0 && matrix[i][j-1] > curr) {
-            int len = 1+LongestIncreasingPath(matrix, i, j-1, cache);
-            max_path = max(max_path, len);
-        }
-        
-        if (j < col-1 && matrix[i][j+1] > curr) {
-            int len = 1+LongestIncreasingPath(matrix, i, j+1, cache);
-            max_path = max(max_path, len);
-        }
-        
-        cache[i][j] = max_path;
-        return max_path;
+        cache[i][j] = max_len;
+        return max_len;
     }
+    
+    vector<vector<int>> dirs = {{-1,0}, {1,0}, {0,1}, {0,-1}};
 };
