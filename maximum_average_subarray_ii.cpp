d98e1c003a0a54c88ab646217ec1b0c87699d7ae
Qingfei.You <qingfeiyou@gmail.com>
2017-09-16 22:37:34 +0200

    feat(cpp): add binary search solution for maximum average subarray ii

--- maximum_average_subarray_ii.cpp
+++ maximum_average_subarray_ii.cpp
@@ -1,3 +1,48 @@
+// O(Nlog(max-min))
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double left = INT_MIN, right = INT_MAX;
+        while (right -left > 0.000004) {
+            double mid = left + (right-left)/2;
+            cout << mid << ' ';
+            if (isValid(nums, k, mid)) left = mid;
+            else right = mid;
+        }
+        return right;
+    }
+    
+private:
+    bool isValid(vector<int>& nums, int k, double x) {
+        int len = nums.size();
+        vector<double> cums(len);
+        for (int i = 0; i < len; ++i) {
+            cums[i] = nums[i]-x;
+        }
+        
+        double curr = 0, last = 0;
+        for (int i = 0; i < k; ++i) {
+            curr += cums[i];
+        }
+        if (curr >= 0) return true;
+        
+        for (int i = k; i < len; ++i) {
+            curr += cums[i];
+            last += cums[i-k];
+            if (last < 0) {
+                curr -= last;
+                last = 0;
+            }
+            
+            if (curr >= 0) return true;
+        }
+        return false;
+    }
+};
+
+
+
+// O(N^2)
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
