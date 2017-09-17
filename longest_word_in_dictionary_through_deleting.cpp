c3f2cd0f8547fe0127ed46f9dc892c13ee4658f5
Qingfei.You <qingfeiyou@gmail.com>
2017-09-17 14:31:34 +0200

    feat(cpp): add n*len solution for longest word in dictionary through deleting

--- longest_word_in_dictionary_through_deleting.cpp
+++ longest_word_in_dictionary_through_deleting.cpp
@@ -1,3 +1,37 @@
+// O(N*len)
+class Solution {
+public:
+    string findLongestWord(string s, vector<string>& d) {
+        string result = "";
+        for (auto word : d) {
+            if (isSubsequence(word, s) && (word.size() > result.size() || (word.size() == result.size() && word < result))) {
+                result = word;
+            }
+        }
+        return result;
+    }
+    
+private:
+    bool isSubsequence(string word, string s) {
+        int len_w = word.size(), len_s = s.size();
+        if (len_w > len_s) return false;
+        
+        int i = 0, j = 0;
+        while (i < len_w) {
+            if (word[i] == s[j]) {
+                ++i; ++j;
+            } else {
+                if (j < len_s) ++j;
+                else break;
+            }
+        }
+        return i == len_w;
+    }
+};
+
+
+
+// O(NlogN * len)
 class Solution {
 public:
     string findLongestWord(string s, vector<string>& d) {
