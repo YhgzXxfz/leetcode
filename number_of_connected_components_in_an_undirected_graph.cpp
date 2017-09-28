bf9af18bbe9f1ef39636309df52adc12b94ddf74
Qingfei.You <qingfeiyou@gmail.com>
2017-09-29 00:30:30 +0200

    feat(cpp): add dfs and bfs solution for number of connected components in an undirected graph

--- number_of_connected_components_in_an_undirected_graph.cpp
+++ number_of_connected_components_in_an_undirected_graph.cpp
@@ -1,3 +1,4 @@
+// union-find
 class Solution {
 public:
     int countComponents(int n, vector<pair<int, int>>& edges) {
@@ -25,4 +26,68 @@ private:
         }
         return id;
     }
+};
+
+// dfs
+class NumberOfConnectedComponentsInAnUndirectedGraph {
+public:
+	int countComponents(int n, vector<pair<int, int>>& edges) {
+		unordered_map<int, unordered_set<int>> graph;
+		for (auto edge : edges) {
+			graph[edge.first].insert(edge.second);
+			graph[edge.second].insert(edge.first);
+		}
+
+		int count = 0;
+		vector<int> visited(n, 0);
+		for (int i = 0; i < n; ++i) {
+			if (!visited[i]) {
+				dfs(graph, visited, i);
+				++count;
+			}
+		}
+		return count;
+	}
+
+private:
+	void dfs(unordered_map<int, unordered_set<int>>& graph, vector<int>& visited, int curr) {
+		if (visited[curr]) return ;
+
+		visited[curr] = true;
+		for (auto neighbor : graph[curr]) {
+			dfs(graph, visited, neighbor);
+		}
+	}
+};
+
+
+// bfs
+class NumberOfConnectedComponentsInAnUndirectedGraph {
+public:
+	int countComponents(int n, vector<pair<int, int>>& edges) {
+		unordered_map<int, unordered_set<int>> graph;
+		for (auto edge : edges) {
+			graph[edge.first].insert(edge.second);
+			graph[edge.second].insert(edge.first);
+		}
+
+		int count = 0;
+		vector<int> visited(n,0);
+		for (int i = 0; i < n; ++i) {
+			if (visited[i]) continue;
+
+			queue<int> q;
+			q.push(i);
+			while (!q.empty()) {
+				auto curr = q.front();
+				q.pop();
+				visited[curr] = true;
+				for (auto neighbor : graph[curr]) {
+					if (!visited[neighbor]) q.push(neighbor);
+				}
+			}
+			++count;
+		}
+		return count;
+	}
 };
