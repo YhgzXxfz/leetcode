c587d94e995739a7ff6502fd181d5a04f164f506
Qingfei.You <qingfeiyou@gmail.com>
2017-10-16 21:26:58 +0200

    refactor(cpp): make hit action in common for lfu cache

--- lfu_cache.cpp
+++ lfu_cache.cpp
@@ -1,58 +1,52 @@
+// O(1)
 class LFUCache {
 public:
-    LFUCache(int capacity):_capacity(capacity), size(0) {
-        
-    }
-    
-    int get(int key) {
-        if (cache.find(key) == cache.end()) return -1;
-        
-        int value = cache[key].first, frequency = cache[key].second;
-        f[frequency].erase(iter[key]);
-        
-        frequency++;
-        cache[key].second = frequency;
-        f[frequency].push_back(key);
-        iter[key] = --f[frequency].end();
-        
-        if (f[min_freq].size() == 0) min_freq = frequency;
-        
-        return value;
-    }
-    
-    void set(int key, int value) {
-        if (_capacity <= 0) return;
-        
-        
-        if (get(key) != -1) {
-            cache[key].first = value;
-            return ;
-        }
-        
-        if (size >= _capacity) {
-            cache.erase(f[min_freq].front());
-            iter.erase(f[min_freq].front());
-            f[min_freq].pop_front();
-            size--;
-        }
-        
-        cache[key] = make_pair(value, 1);
-        f[1].push_back(key);
-        iter[key] = --f[1].end();
-        min_freq = 1;
-        size++;
-    }
+	LFUCache(int capacity) : _capacity(capacity), size(0), min_occur(0) {}
+
+	int get(int key) {
+		auto it = cache.find(key);
+		if (it == cache.end()) return -1;
+
+		hit(it);
+		return it->second.first;
+	}
+
+	void put(int key, int value) {
+		if (_capacity <= 0) return ;
+
+		auto it = cache.find(key);
+		if (it != cache.end()) {
+			hit(it);
+			it->second.first = value;
+		} else {
+			if (size >= _capacity) {
+				cache.erase(freq[min_occur].front());
+				iter.erase(freq[min_occur].front());
+				freq[min_occur].pop_front();
+				--size;
+			}
+
+			cache[key] = make_pair(value, 1);
+			freq[1].push_back(key);
+			iter[key] = prev(freq[1].end());
+			min_occur = 1;
+			++size;
+		}
+	}
 
 private:
-    int _capacity, size, min_freq;
-    unordered_map<int, pair<int, int>> cache;
-    unordered_map<int, list<int>> f;
-    unordered_map<int, list<int>::iterator> iter;
-};
+	void hit(unordered_map<int, pair<int, int>>::iterator it) {
+		int key = it->first, occur = it->second.second;
+		freq[occur].erase(iter[key]);
+		it->second.second++;
+		freq[it->second.second].push_back(key);
+		iter[key] = prev(freq[it->second.second].end());
 
-/**
- * Your LFUCache object will be instantiated and called as such:
- * LFUCache obj = new LFUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.set(key,value);
- */
+		if (freq[min_occur].size() == 0) ++min_occur;
+	}
+
+	int _capacity, size, min_occur;
+	unordered_map<int, pair<int, int>> cache;
+	unordered_map<int, list<int>> freq;
+	unordered_map<int, list<int>::iterator> iter;
+};
