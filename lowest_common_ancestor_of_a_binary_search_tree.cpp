0bd445615d47b3109537de190d64df6fbb7c3b03
Qingfei.You <qingfeiyou@gmail.com>
2017-10-25 20:21:03 +0200

    feat(cpp): add iterative solution for lowest common ancestor of a binary search tree

--- lowest_common_ancestor_of_a_binary_search_tree.cpp
+++ lowest_common_ancestor_of_a_binary_search_tree.cpp
@@ -16,3 +16,36 @@ public:
         else return root;
     }
 };
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || !p || !q) return nullptr;
+
+		auto path_p = getPath(root, p), path_q = getPath(root, q);
+		auto lca = root;
+		while (!path_p.empty() && !path_q.empty()) {
+			auto curr_p = path_p.front();
+			path_p.pop();
+			auto curr_q = path_q.front();
+			path_q.pop();
+			if (curr_p != curr_q) return lca;
+			lca = curr_p;
+		}
+		return lca;
+	}
+
+private:
+	queue<TreeNode*> getPath(TreeNode* root, TreeNode* p) {
+		queue<TreeNode*> q;
+		auto curr = root;
+		while (curr) {
+			q.push(curr);
+			if (p->val == curr->val) break;
+			else if (p->val < curr->val) curr = curr->left;
+			else curr = curr->right;
+		}
+		return q;
+    }
+};
