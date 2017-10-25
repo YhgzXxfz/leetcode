d9548439eb38326af845cbe288666de17546259a
Qingfei.You <qingfeiyou@gmail.com>
2017-10-25 20:07:57 +0200

    feat(cpp): add solution for binary tree level order traversal ii

--- binary_tree_level_order_traversal_ii.cpp
+++ binary_tree_level_order_traversal_ii.cpp
@@ -11,6 +11,34 @@ class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
         vector<vector<int>> result;
+		if (!root) return result;
+
+		queue<TreeNode*> q;
+		q.push(root);
+		int d = -1;
+		while (!q.empty()) {
+			int size = q.size();
+			result.push_back(vector<int>());
+			++d;
+			for (int i = 0; i < size; ++i) {
+				auto curr = q.front();
+				q.pop();
+
+				result[d].push_back(curr->val);
+				if (curr->left) q.push(curr->left);
+				if (curr->right) q.push(curr->right);
+			}
+		}
+        reverse(result.begin(), result.end());
+		return result;
+    }
+};
+
+
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> result;
         bfs(root, result, 1);
         reverse(result.begin(), result.end());
         return result;
