9e7344f2d28699a65158f75fffec3176471d6571
Qingfei.You <qingfeiyou@gmail.com>
2017-10-26 22:10:30 +0200

    feat(cpp): add iterative solutions for binary tree paths

--- binary_tree_paths.cpp
+++ binary_tree_paths.cpp
@@ -7,6 +7,55 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// stack
+class Solution {
+public:
+	vector<string> binaryTreePaths(TreeNode* root) {
+		vector<string> result;
+		if (!root) return result;
+
+		stack<pair<TreeNode*, string>> s;
+		s.push(make_pair(root, ""));
+		while (!s.empty()) {
+			auto curr = s.top();
+			s.pop();
+			auto node = curr.first;
+			string path = curr.second;
+			if (!node->left && !node->right) result.push_back(path+to_string(node->val));
+			else {
+				if (node->left) s.push(make_pair(node->left, path+to_string(node->val)+"->"));
+				if (node->right) s.push(make_pair(node->right, path+to_string(node->val)+"->"));
+			}
+		}
+		return result;
+	}
+};
+
+// queue
+class Solution {
+public:
+	vector<string> binaryTreePaths(TreeNode* root) {
+		vector<string> result;
+		if (!root) return result;
+
+		queue<pair<TreeNode*, string>> q;
+		q.push(make_pair(root, ""));
+		while (!q.empty()) {
+			auto curr = q.front();
+			q.pop();
+			auto node = curr.first;
+			string path = curr.second;
+			if (!node->left && !node->right) result.push_back(path+to_string(node->val));
+			else {
+				if (node->left) q.push(make_pair(node->left, path+to_string(node->val)+"->"));
+				if (node->right) q.push(make_pair(node->right, path+to_string(node->val)+"->"));
+			}
+		}
+		return result;
+	}
+};
+
+// dfs
 class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
