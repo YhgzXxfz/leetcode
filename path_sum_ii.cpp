ae22e5b4a7c99fc5ba9e0a03b5b4bd8925a04c2a
Qingfei.You <qingfeiyou@gmail.com>
2017-10-26 23:03:37 +0200

    feat(cpp): add iterative solution for path sum ii

--- path_sum_ii.cpp
+++ path_sum_ii.cpp
@@ -7,6 +7,44 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// iterative
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>> result;
+		vector<TreeNode*> s;
+		int total = 0;
+		TreeNode* curr = root, *prev = nullptr;
+		while (curr || !s.empty()) {
+			while (curr) {
+				s.push_back(curr);
+				total += curr->val;
+				curr = curr->left;
+			}
+			curr = s.back();
+			if (!curr->left && !curr->right && total == sum) {
+				vector<int> temp;
+				for (auto node : s) {
+					temp.push_back(node->val);
+				}
+				result.push_back(temp);
+			}
+
+			if (curr->right && curr->right != prev) {
+				curr = curr->right;
+			} else {
+				prev = curr;
+				total -= curr->val;
+				s.pop_back();
+				curr = nullptr;
+			}
+		}
+		return result;
+    }
+};
+
+
+// dfs
 class Solution {
 public:
     vector<vector<int>> pathSum(TreeNode* root, int sum) {
