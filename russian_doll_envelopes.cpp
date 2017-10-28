8c29049b43555d4323d760e39a0e905cb132c1de
Qingfei.You <qingfeiyou@gmail.com>
2017-10-28 21:24:44 +0200

    feat(cpp): add O(NlogN) solution for russian doll envelopes

--- russian_doll_envelopes.cpp
+++ russian_doll_envelopes.cpp
@@ -1,3 +1,30 @@
+// O(NlogN)
+class Solution {
+public:
+    int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+        if (envelopes.empty()) return 0;
+
+		int len = envelopes.size();
+		if (len == 1) return 1;
+
+		auto comp = [&](pair<int,int> p1, pair<int,int> p2) {
+			if (p1.first == p2.first) return p1.second > p2.second;
+			return p1.first < p2.first;
+		};
+		sort(envelopes.begin(), envelopes.end(), comp);
+
+		vector<int> dp(len), height(len+1, INT_MAX);
+		for (int i = 0; i < len; ++i) {
+			int k = lower_bound(height.begin(), height.end(), envelopes[i].second)-height.begin();
+			dp[i] = k;
+			height[k] = envelopes[i].second;
+		}
+		return 1 + *max_element(dp.begin(), dp.end());
+    }
+};
+
+
+// O(N^2)
 class Solution {
 public:
     int maxEnvelopes(vector<pair<int, int>>& envelopes) {
